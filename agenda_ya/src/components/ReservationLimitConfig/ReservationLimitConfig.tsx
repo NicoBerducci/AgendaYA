b735db7f759bbe49248c4fd41f843c0bed7aa67f
MartinGenaulaz05 <lgenaulaz@gmail.com>
2026-09-21 00:13:58 -0300

    Revert "Agregación de atributos data-cy al frontend y configuración cypress"
    This reverts commit d92e91e0939af5d5552ee16fe15605efa62273ab.

--- agenda_ya/src/components/ReservationLimitConfig/ReservationLimitConfig.tsx
+++ agenda_ya/src/components/ReservationLimitConfig/ReservationLimitConfig.tsx
@@ -204,7 +204,6 @@ export const ReservationLimitConfig: React.FC<ReservationLimitConfigProps> = ({ 
             </label>
             <div style={{ display: 'flex', gap: 12, alignItems: 'center' }}>
               <input
-                data-cy="daily-limit-input"
                 type="number"
                 placeholder="Ej: 02"
                 value={limitInput}
@@ -249,7 +248,6 @@ export const ReservationLimitConfig: React.FC<ReservationLimitConfigProps> = ({ 
               open={true}
               tipo={isSuccess ? 'ok' : 'err'}
               texto={feedbackMessage}
-              dataCy={isSuccess ? 'success-message' : 'error-message'}
               onClose={() => setFeedbackMessage(null)}
               botones={
                 !isSuccess ? (
@@ -294,7 +292,6 @@ export const ReservationLimitConfig: React.FC<ReservationLimitConfigProps> = ({ 
             Cancelar
           </button>
           <button
-            data-cy="save-daily-limit-button"
             onClick={handleSave}
             disabled={loading}
             style={{
